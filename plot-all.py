e01fa1dc8ca108ba55472c0df88f17c1720792d0
Tian-Bai <tian.bai3@mail.mcgill.ca>
2024-09-19 17:51:57 -0400

    boxplot

--- plot-all.py
+++ plot-all.py
@@ -124,3 +124,53 @@ plt.savefig(os.path.join("pic", "power.png"))
 # plt.show()
 
 ''''''''''''
+# Assume dataset_list is already defined
+df_list = []
+
+for name in dataset_list:
+    df_ones = []
+    for j in range(1, 101):
+        df = pd.read_csv(os.path.join("result", f"{name} 0.10", f"{name} 0.10 {j}.csv"))
+        df = df[df['fdp_nominals'] == 0.2]
+        df_ones.append(df)
+    df = pd.concat(df_ones)
+    df_list.append(df)
+
+fig, axs = plt.subplots(nrows=3, ncols=5, figsize=(18, 12))
+axs = axs.flatten()
+
+# Define colors for the three boxplots
+colors = ['steelblue', 'orange', 'darkgreen']
+
+for i, name in enumerate(dataset_list):
+    ax = axs[i]
+
+    # Prepare data for the 3 boxplots (each array separately)
+    data = pd.DataFrame({
+        'bin': df_list[i]['fdps_15_rb'],
+        'pred': df_list[i]['fdps_15_rp'],
+        'cs': df_list[i]['fdps_cs']
+    })
+
+    # Create boxplot using seaborn
+    sns.boxplot(data=data, ax=ax, palette=colors, showfliers=True, width=0.5, showmeans=True, 
+                meanprops={'markerfacecolor':'red', 
+                          'markeredgecolor':'red'})
+    
+    ax.axhline(y=0.2, color='grey', linestyle='-.')
+
+    # Set title
+    ax.set_title(f'{name}', fontsize=12)
+
+    # Customize grid
+    ax.grid(True, linestyle='--', alpha=0.7)
+
+# Adjust spacing between subplots
+fig.subplots_adjust(wspace=0.2, hspace=0.3, top=0.9, bottom=0.13, left=0.07, right=0.96)
+
+# Add global x and y labels, move them slightly outward
+fig.text(0.5, 0.07, 'Observed FDP', ha='center', fontsize=14)
+fig.text(0.03, 0.5, 'Selection Methods', va='center', rotation='vertical', fontsize=14)
+
+# Save the plot to a file
+plt.savefig(os.path.join("pic", "fdp-box.png"))
