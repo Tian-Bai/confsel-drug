1f0e09477c7837a7582e4b0591a71d3d63a077c3
Tian-Bai <tian.bai3@mail.mcgill.ca>
2024-09-26 20:56:13 -0400

    scripts

--- job.sh
+++ job.sh
@@ -1,12 +1,40 @@
 #!/bin/bash
 
+# Slurm parameters
+MEMO=25G                     # Memory required (50G)
+TIME=3-00:00:00              # Time required (3d)
+CORE=10                      # Cores required (10)
+
+# Assemble order prefix
+ORDP="sbatch --mem="$MEMO" -n 1 -c "$CORE" -p "$PART" --time="$TIME" --chdir=/scratch/users/ying531/opt-cs/simulation/"
+
+# Create directory for log files
+LOGS=logs
+mkdir -p $LOGS
+
 dataset_list=("3A4" "CB1" "DPP4" "HIVINT" "HIVPROT" "LOGD" "METAB" "NK1" "OX1" "OX2" "PGP" "PPB" "RAT_F" "TDI" "THROMBIN")
 
-for dataset in "3A4"; do
-    for size in 1.0; do
-        for seed in {1..50}; do
-            echo "running $dataset $size $seed"
-            python3 sheridan-vs-conformal.py $dataset $size $seed &
+for dataset in "${dataset_list[@]}"; do
+    for size in 0.1 1.0; do
+        for seed in {1..10}; do
+            # Assemble slurm order for this job
+            JOBN=$dataset"_"$size"_"$seed
+
+            # Submit the job
+            SCRIPT="submit.sh $dataset $size $seed"
+                
+            OUTF=$LOGS"/"$JOBN".out"
+            ERRF=$LOGS"/"$JOBN".err"
+
+            # Assemble slurm order for this job
+            ORD=$ORDP" -J "$JOBN" -o "$OUTF" -e "$ERRF" "$SCRIPT
+            # ORD="scancel -n "$JOBN  
+
+            # Print order
+            echo $ORD
+
+            # Submit order
+            $ORD
         done
     done
 done
---
+++ submit.sh
@@ -0,0 +1,3 @@
+#!/bin/bash 
+
+python sheridan-vs-conformal.py $1 $2 $3
